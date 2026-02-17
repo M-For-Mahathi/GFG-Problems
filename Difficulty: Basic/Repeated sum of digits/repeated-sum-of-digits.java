bce084db572651a8228a24176442d9ec11c44d53
M-For-Mahathi <163542249+M-For-Mahathi@users.noreply.github.com>
2026-02-17 12:01:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Repeated sum of digits/repeated-sum-of-digits.java
@@ -0,0 +1,8 @@
+// User function Template for Java
+class Solution {
+    static int repeatedSumOfDigits(int n) {
+        if (n == 0) return 0;
+        if (n % 9 == 0) return 9;
+        return (n % 9);
+    }
+}
